dfd32b9d54f07594c0d714a1dd6e0b17f186eb20
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2020-02-11 15:23:39 -0500

    update code for image rendering

--- src/Components/InstrumentCard/InstrumentCard.js
+++ src/Components/InstrumentCard/InstrumentCard.js
@@ -4,38 +4,16 @@ import "./InstrumentCard.css";
 
 export default function InstrumentCard(props) {
   const { title } = props;
-  let imageLink = "";
 
-  if (title) {
-    switch (title) {
-      case "guitar":
-        imageLink = process.env.PUBLIC_URL + "/Photos/guitar-cropped.jpg";
-        break;
-
-      case "piano":
-        imageLink = process.env.PUBLIC_URL + "/Photos/piano-resize.jpg";
-        break;
-
-        case 'clarinet':
-          imageLink = process.env.PUBLIC_URL + "/Photos/clarinet.jpg";
-          break;
-
-          case 'violin':
-
-          imageLink = process.env.PUBLIC_URL + "/Photos/violin.jpg";
-          break;
-
-      default:
-        imageLink = "";
-    }
-  }
-
+  //use dynamic import for images
   return (
     <div className={`InstrumentCard`}>
       <Link to={`/category/instrument/${title}`}>
         <div className="img-wrap">
-          {/* {imageLink.length > 0 ? <img src={imageLink} alt={title} /> : ""} */}
-          <img src={imageLink} alt={title} />
+          <img
+            src={`${require(`../../Assets/images/${title}.jpg`)}`}
+            alt={title}
+          />
         </div>
 
         <div className="InstrumentCard__title">
