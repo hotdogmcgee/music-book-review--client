bab747d87efc9b07ecd6c253ca888ef3d9668793
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2020-02-25 14:00:59 -0500

    clean code

--- src/Components/InstrumentCard/InstrumentCard.test.js
+++ src/Components/InstrumentCard/InstrumentCard.test.js
@@ -3,7 +3,7 @@ import InstrumentCard from "./InstrumentCard";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 
-describe.only("InstrumentCard component", () => {
+describe("InstrumentCard component", () => {
   it("renders a InstrumentCard by default", () => {
     const wrapper = shallow(<InstrumentCard title="guitar" />);
     expect(toJson(wrapper)).toMatchSnapshot();
